49a4049835d26149092407b6f7e7a0fa1dcc8661
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-07-12 08:21:42 +0700

    Create 049.c

---
+++ 049.c
@@ -0,0 +1,68 @@
+/* - Chuong trinh minh hoa chuyen dong cua mot trai pinball */
+#include <conio.h>
+#include <graphics.h>
+#include <dos.h>
+#include <stdlib.h>
+
+#define RADIUS 4
+#define BALLCOLOR 14
+#define BACKCOLOR 0
+#define DELAY 10
+
+void run_pinball()
+{
+  int dx, dy, x, y, dung = 0;
+
+  rand();
+  do {
+    dx = rand() % 3- 1;
+  } while (dx == 0);
+  do {
+    dy = rand()% 3 - 1;
+  } while (dy == 0);
+  x = rand() % getmaxx();
+  y = rand() % getmaxy();
+
+  rectangle(0, 0, getmaxx(), getmaxy());
+  do {
+    setcolor(BALLCOLOR);
+    setfillstyle(SOLID_FILL, BALLCOLOR);
+    fillellipse(x, y, RADIUS, RADIUS);
+    delay(DELAY);
+    setcolor(BACKCOLOR);
+    setfillstyle(SOLID_FILL, BACKCOLOR);
+    fillellipse(x, y, RADIUS, RADIUS);
+    x += dx;
+    y += dy;
+    if (x < RADIUS +1 || x > getmaxx() - RADIUS - 1)
+    {
+      dx = -dx;
+      x += 2 * dx;
+      dung = 1;
+    }
+    if (y < RADIUS + 1 || y > getmaxy() - RADIUS - 1)
+    {
+      dy = -dy;
+      y += 2 * dy;
+      dung = 1;
+    }
+    if (dung)
+    {
+      //sound (1000);
+      delay(50);
+      //nosound();
+      dung = 0;
+    }
+  } while (!kbhit());
+}
+
+int main()
+{
+  int gr_drive = DETECT, gr_mode;
+
+  initgraph(&gr_drive, &gr_mode, "");
+
+  run_pinball();
+  getch();
+  closegraph();
+}
