ba02b0945074c3158ddc73a2b089048a29f8ee1c
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-07-12 08:03:23 +0700

    Create 016.c

---
+++ 016.c
@@ -0,0 +1,33 @@
+/* - Chuong trinh ve duong Cardioid */
+#include <conio.h>
+#include <graphics.h>
+#include <math.h>
+
+#define K 150
+
+void cardioid()
+{
+   double goc = 0.0;
+   double f, xc, yc;
+
+   xc = getmaxx() / 2;
+   yc = getmaxy() / 2;
+   while (!kbhit())
+   {
+      f = K * (1 + cos(goc));
+      putpixel(xc + f * cos(goc), yc + f * sin(goc), YELLOW);
+      goc += 0.01;
+   }
+}
+
+int main()
+{
+  int gr_drive = DETECT, gr_mode;
+
+  initgraph(&gr_drive, &gr_mode, "");
+  cardioid();
+
+  getch();
+  closegraph();
+  return 0;
+}
